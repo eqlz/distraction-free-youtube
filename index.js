c4351cfa2f21a188d9979ee6e57169392b702093
Qinglu Zhang <qingluzhang@Qinglus-MacBook-Pro.local>
2020-07-30 04:41:44 -0400

    add url parse

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const BASE_URL = 'https://www.youtube.com/embed/';
+
+const getVideoIdFromUrl = (url) => {
+    return url.split('?v=')[1];
+}
+
 const playButtonEle = document.getElementById("play-button");
 
 playButtonEle.addEventListener("click", () => {
     const videoUrl = document.getElementById("video-url").value;
+    const videoId = getVideoIdFromUrl(videoUrl);
+    const embeddedVideoUrl = BASE_URL + videoId;
 
     const iframeEle = document.getElementById("yt-player");
-    iframeEle.setAttribute("src", videoUrl);
+    iframeEle.setAttribute("src", embeddedVideoUrl);
 });
