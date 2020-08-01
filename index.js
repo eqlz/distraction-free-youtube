a51af6a55d5bcbf16cb7d5575bcafe056c2da2a0
Qinglu Zhang <qinglu.zh@gmail.com>
2020-08-01 02:29:30 -0400

    get parameter playlist

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'https://www.youtube.com/embed/';
 const PARAMETER_LOOP = 'loop=1'
+const getParameterPlaylist = (videoId) => `playlist=${videoId}`;
 
 const getVideoIdFromUrl = (url) => {
     return url.split('?v=')[1];
@@ -10,7 +11,9 @@ const playButtonEle = document.getElementById("play-button");
 playButtonEle.addEventListener("click", () => {
     const videoUrl = document.getElementById("video-url").value;
     const videoId = getVideoIdFromUrl(videoUrl);
-    const embeddedVideoUrl = BASE_URL + videoId + '?' + PARAMETER_LOOP;
+    const embeddedVideoUrl = BASE_URL + videoId + '?' +
+        getParameterPlaylist(videoId) + '&' +
+        PARAMETER_LOOP;
 
     const iframeEle = document.getElementById("yt-player");
     iframeEle.setAttribute("src", embeddedVideoUrl);
