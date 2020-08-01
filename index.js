42e69926cac46f1f53950c39d2125459d1914b0c
Qinglu Zhang <qinglu.zh@gmail.com>
2020-08-01 02:50:23 -0400

    add param playlist

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const BASE_URL = 'https://www.youtube.com/embed/';
-const PARAMETER_LOOP = 'loop=1'
-const getParameterPlaylist = (videoId) => `playlist=${videoId}`;
+
+const getParams = (parametersList) => {
+    return parametersList.reduce((result, parameter, index) => {
+        if (index === 0) {
+            return result += parameter.key + '=' + parameter.value;
+        }
+        return result += '&' + parameter.key + '=' + parameter.value;
+    }, '');
+}
+
 
 const getVideoIdFromUrl = (url) => {
     return url.split('?v=')[1];
@@ -11,9 +19,17 @@ const playButtonEle = document.getElementById("play-button");
 playButtonEle.addEventListener("click", () => {
     const videoUrl = document.getElementById("video-url").value;
     const videoId = getVideoIdFromUrl(videoUrl);
-    const embeddedVideoUrl = BASE_URL + videoId + '?' +
-        getParameterPlaylist(videoId) + '&' +
-        PARAMETER_LOOP;
+    const parametersList = [
+        {
+            key: 'playlist',
+            value: videoId,
+        },
+        {
+            key: 'loop',
+            value: 1,
+        }
+    ];
+    const embeddedVideoUrl = BASE_URL + videoId + '?' + getParams(parametersList);
 
     const iframeEle = document.getElementById("yt-player");
     iframeEle.setAttribute("src", embeddedVideoUrl);
