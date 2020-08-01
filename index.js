0936c96ffd422a01139d20de15bdbb498e615a15
Qinglu Zhang <qinglu.zh@gmail.com>
2020-08-01 02:13:32 -0400

    add loop paramter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://www.youtube.com/embed/';
+const PARAMETER_LOOP = 'loop=1'
 
 const getVideoIdFromUrl = (url) => {
     return url.split('?v=')[1];
@@ -9,7 +10,7 @@ const playButtonEle = document.getElementById("play-button");
 playButtonEle.addEventListener("click", () => {
     const videoUrl = document.getElementById("video-url").value;
     const videoId = getVideoIdFromUrl(videoUrl);
-    const embeddedVideoUrl = BASE_URL + videoId;
+    const embeddedVideoUrl = BASE_URL + videoId + '?' + PARAMETER_LOOP;
 
     const iframeEle = document.getElementById("yt-player");
     iframeEle.setAttribute("src", embeddedVideoUrl);
